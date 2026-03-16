2895847fe23c35a874cdb8a2fa71d00df88737e0
SOORYA77 <ksoorya065@gmail.com>
2026-03-16 20:11:03 +0530

    Update calculator.py

--- src/calculator.py
+++ src/calculator.py
@@ -1,1 +1,37 @@
+def add(a, b):
+    return a + b
 
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+
+def main():
+    print("Simple CLI Calculator")
+
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+
+    op = input("Choose operation (+ - * /): ")
+
+    if op == "+":
+        print(add(a, b))
+    elif op == "-":
+        print(subtract(a, b))
+    elif op == "*":
+        print(multiply(a, b))
+    elif op == "/":
+        print(divide(a, b))
+    else:
+        print("Invalid operation")
+
+
+if __name__ == "__main__":
+    main()
