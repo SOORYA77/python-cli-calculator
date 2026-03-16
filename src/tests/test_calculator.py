ed5f82a1d2abcbe62dd005ef6f3018b0d6983a83
SOORYA77 <ksoorya065@gmail.com>
2026-03-16 20:17:56 +0530

    Update test_calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -1,1 +1,13 @@
+from src.calculator import add, subtract, multiply, divide
 
+def test_add():
+    assert add(2,3) == 5
+
+def test_subtract():
+    assert subtract(5,3) == 2
+
+def test_multiply():
+    assert multiply(4,2) == 8
+
+def test_divide():
+    assert divide(10,2) == 5
